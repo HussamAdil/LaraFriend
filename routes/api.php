1d5ccc183ab503421ee38b5d23c6eacc7b6666d5
hussamadil <hussam0683@gmail.com>
2021-05-29 22:35:25 +0200

    install auth package laravelui

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProfileController;
+use Symfony\Component\VarDumper\Caster\ResourceCaster;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,6 +25,14 @@ Route::post('/auth/login', [AuthController::class, 'login']);
 
 Route::post('/auth/logout', [AuthController::class, 'logout']);
 
+// profile 
+
+Route::group(['middleware' => 'api'] , function()
+{
+    Route::apiResource('profile',ProfileController::class);
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+ 
